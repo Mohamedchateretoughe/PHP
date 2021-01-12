b441d1309c4e66bfe5c79b9ec3f7bd2bc0887ffa
Mohamedchateretoughe <77307377+Mohamedchateretoughe@users.noreply.github.com>
2021-01-12 00:43:27 +0000

    Add files via upload

---
+++ carre.php
@@ -0,0 +1,24 @@
+<?php
+
+// coté du carré
+$cducarre = 2 ;
+echo "le cote du carre est de ". $cducarre."</br>";
+
+// perimetre d'un carré
+$pducarre = $cducarre * 4 ;
+echo "le perimetre du carre est de ". $pducarre."</br>";
+
+// Demi-perimetre d'un carré
+$demiperim =  $pducarre /2 ;
+echo "le demi-perimetre du carre est de ". $demiperim."</br>";
+
+// surface d'un carré
+$surface =  $cducarre * $cducarre ;
+echo "la surface du carre est de ". $surface."</br>";
+
+//  la diagonale d'un carré
+$diag =  ($cducarre**2) + ($cducarre**2) ;
+echo "la diag du carre est de ". $diag."</br>";
+
+
+?>
